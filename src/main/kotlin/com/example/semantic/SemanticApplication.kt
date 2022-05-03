891f31ad8dc727f11364a19ef09206de46350737
huisam <huisam@naver.com>
2022-05-04 00:32:30 +0900

    feat: gradle project

---
+++ src/main/kotlin/com/example/semantic/SemanticApplication.kt
@@ -0,0 +1,11 @@
+package com.example.semantic
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SemanticApplication
+
+fun main(args: Array<String>) {
+    runApplication<SemanticApplication>(*args)
+}
